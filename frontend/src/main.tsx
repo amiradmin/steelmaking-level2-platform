15a4121ad99f8dbb06376eafd737303884acfa60
Amir Behvandi <amirbehvandi747@gmail.com>
2026-09-13 15:22:34 +0330

    feat: mount system map PLC telemetry overlay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { LicenseNotice } from './LicenseNotice'
 import { PlcSourceOverlay } from './PlcSourceOverlay'
+import { SystemMapTelemetryOverlay } from './SystemMapTelemetryOverlay'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <LicenseNotice>
       <App />
       <PlcSourceOverlay />
+      <SystemMapTelemetryOverlay />
     </LicenseNotice>
   </React.StrictMode>,
 )

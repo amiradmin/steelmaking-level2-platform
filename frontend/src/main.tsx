28310388ba18203878d1754bbd3ca0043bca818b
Amir Behvandi <amirbehvandi747@gmail.com>
2026-09-13 15:05:55 +0330

    feat: mount PLC source overlay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { LicenseNotice } from './LicenseNotice'
+import { PlcSourceOverlay } from './PlcSourceOverlay'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LicenseNotice>
       <App />
+      <PlcSourceOverlay />
     </LicenseNotice>
   </React.StrictMode>,
 )

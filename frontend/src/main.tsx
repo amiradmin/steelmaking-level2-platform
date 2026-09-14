4a12e8bd18583bb676ea036368625d03507613ec
Amir Behvandi <amirbehvandi747@gmail.com>
2026-09-14 13:42:01 +0330

    feat(frontend): mount raw materials navigation bridge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { LicenseNotice } from './LicenseNotice'
+import { MaterialsNavigationBridge } from './MaterialsNavigationBridge'
 import './styles.css'
 import './production-flow-tooltip-fix.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LicenseNotice>
+      <MaterialsNavigationBridge />
       <App />
     </LicenseNotice>
   </React.StrictMode>,

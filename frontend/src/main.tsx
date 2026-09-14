3c10187ae4784c2aa1bd5123d629b72ebb10fac7
Amir Behvandi <amirbehvandi747@gmail.com>
2026-09-14 08:03:30 +0330

    feat: enable live overview overlay

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { LicenseNotice } from './LicenseNotice'
+import { OverviewLiveOverlay } from './OverviewLiveOverlay'
 import { PlcSourceOverlay } from './PlcSourceOverlay'
 import { SystemMapTelemetryOverlay } from './SystemMapTelemetryOverlay'
 import './styles.css'
-import './production-flow-tooltip-fix.css'
+import './production-flow-tooltip-placement.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <LicenseNotice>
       <App />
+      <OverviewLiveOverlay />
       <PlcSourceOverlay />
       <SystemMapTelemetryOverlay />
     </LicenseNotice>
